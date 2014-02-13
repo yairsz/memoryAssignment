345f69a9ce3fc8112b635a4febd2822f0e5af81c
Yair Szarf <yair@mac.com>
2014-02-13 12:35:11 -0800

    Initial Commit

---
+++ memoryAssignmet/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  memoryAssignmet
+//
+//  Created by Yair Szarf on 2/13/14.
+//  Copyright (c) 2014 The 2 Handed Consortium. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
